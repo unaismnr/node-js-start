ee4639b8ff2106b7fcd72b2f1baa385384fe6965
Billme Cart <mnrunais@gmail.com>
2024-06-13 19:07:27 +0530

    update

---
+++ sample.js
@@ -0,0 +1,5 @@
+var Name = 'Netecart'
+var n = 34
+
+console.log('Node js Starter: ' + Name + ' Number: ' + 34)
+console.log(Name.length)
---
+++ sum.js
@@ -0,0 +1,13 @@
+var a = 10
+var b = 20
+
+var sum = a + b
+console.log(sum)
+
+var nums = [20, 20.5, 5.3, 4, 6]
+
+var sum = 20 + 20.5 + 5.3 + 4 + 6
+
+var average = sum / nums.length
+
+console.log(average)
